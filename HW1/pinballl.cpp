1db980e719d2affbd5149d44aae2c0588733ceff
Blake Williams <blaker1703@gmail.com>
2023-02-01 12:50:59 -0600

    modified:   HW1/pinballl.cpp

---
+++ HW1/pinballl.cpp
@@ -0,0 +1,59 @@
+#include "pinball.h"
+#include <iostream>
+using namespace std; 
+
+
+//Constructors
+    Pinball::Pinball(){
+        
+
+    }
+   Pinball:: Pinball(const string mach_name,const int year_prod, const string manu, const float fun_rating, const string theme  ){
+
+   }
+    Pinball::Pinball(const Pinball & copy){
+
+    }
+    Pinball::~Pinball(){
+
+    }
+
+
+// Utility Methods 
+    void Pinball::print() const{
+
+    }
+
+//Getters
+    string Pinball::getMach_name() const{
+
+    }
+    int Pinball::getYear_prod() const{
+
+    }
+    string Pinball::getManu() const{
+
+    }
+    float Pinball::getFun_rating() const{
+
+    }
+    string Pinball::getTheme() const{
+
+    }
+// Setters
+    void Pinball::setMach_name(const string mach_name){
+
+    }
+    void Pinball::setYear_prod(const int year_prod){
+
+    }
+    void Pinball::setManu(const string manu){
+
+    }
+    void Pinball::setFun_rating(const float fun_rating){
+
+    }
+    void Pinball::setTheme(const string theme){
+
+    }
+    
