6b5040c20a3589fb63eb395807411e7b5a9c652b
Blake Williams <blaker1703@gmail.com>
2023-02-01 13:05:34 -0600

    modified:   HW1/pinballl.cpp

--- HW1/pinballl.cpp
+++ HW1/pinballl.cpp
@@ -43,34 +43,34 @@ using namespace std;
 
 //Getters
     string Pinball::getMach_name() const{
-
+        return mach_name;
     }
     int Pinball::getYear_prod() const{
-
+        return year_prod;
     }
     string Pinball::getManu() const{
-
+        return manu;
     }
     float Pinball::getFun_rating() const{
-
+        return fun_rating;
     }
     string Pinball::getTheme() const{
-
+        return theme;
     }
 // Setters
-    void Pinball::setMach_name(const string mach_name){
-
+    void Pinball::setMach_name(const string mach_nameIn){
+        mach_name = mach_nameIn;
     }
-    void Pinball::setYear_prod(const int year_prod){
-
+    void Pinball::setYear_prod(const int year_prodIn){
+        year_prod = year_prodIn;
     }
-    void Pinball::setManu(const string manu){
-
+    void Pinball::setManu(const string manuIn){
+        manu = manuIn; 
     }
-    void Pinball::setFun_rating(const float fun_rating){
-
+    void Pinball::setFun_rating(const float fun_ratingIn){
+        fun_rating = fun_ratingIn;
     }
-    void Pinball::setTheme(const string theme){
-
+    void Pinball::setTheme(const string themeIn){
+        theme = themeIn; 
     }
     
